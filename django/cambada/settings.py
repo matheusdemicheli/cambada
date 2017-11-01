d395eaf2af90200d2243411c0835059aad38b901
Matheus Monego Demicheli <matheusdemicheli@gmail.com>
2017-11-01 18:50:12 +0000

    Adicionado ALLOW_HOSTS

--- django/cambada/settings.py
+++ django/cambada/settings.py
@@ -8,6 +8,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -24,7 +26,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['matheusmonego.pythonanywhere.com']
 
 
 # Application definition
