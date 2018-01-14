35a23c32752290f2851451f0fba63e10016a78d7
Matheus Monego Demicheli <matheusdemicheli@gmail.com>
2018-01-14 21:09:36 +0000

    Adicionada URL e view para pesquisa de frases

--- django/frases_historicas/urls.py
+++ django/frases_historicas/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('frases_historicas.views',
     url(r'^aleatoria/$', 'get_frase_aleatoria', name='get_frase_aleatoria'),
     url(r'^dia/$', 'get_frase_do_dia', name='get_frase_do_dia'),
-    # url(r'^pesquisa/$', 'pesquisar_frase', name='pesquisar_frase'),
+    url(r'^pesquisa/$', 'pesquisar_frases', name='pesquisar_frases'),
 )
--- django/frases_historicas/views.py
+++ django/frases_historicas/views.py
@@ -1,4 +1,6 @@
 #-*- coding: utf-8 -*-
+from django.core.paginator import Paginator, EmptyPage
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from frases_historicas.models import FraseHistorica
 
@@ -8,12 +10,7 @@ def get_frase_aleatoria(request):
     Retorna uma frase aleatória.
     """
     frase = FraseHistorica.objects.get_frase_aleatoria()
-    dados = {
-        'autor': frase.autor,
-        'data': frase.data,
-        'texto': frase.texto
-    }
-    return JsonResponse(dados)
+    return JsonResponse(model_to_dict(frase))
 
 
 def get_frase_do_dia(request):
@@ -21,16 +18,25 @@ def get_frase_do_dia(request):
     Retorna a frase do dia.
     """
     frase = FraseHistorica.objects.get_frase_do_dia()
-    dados = {
-        'autor': frase.autor,
-        'data': frase.data,
-        'texto': frase.texto
-    }
-    return JsonResponse(dados)
+    return JsonResponse(model_to_dict(frase))
 
 
-# def filter_pesquisa_frases(request):
-#     """
-#     Retorna as frases que contém o termo pesquisado.
-#     """
-#     frases = FraseHistorica.objects.filter(texto__icontains=termo)
+def pesquisar_frases(request):
+    """
+    Retorna as frases que contém o termo pesquisado.
+    """
+    termo = request.GET.get('termo')
+    numero_pagina = request.GET.get('pagina')
+
+    frases = FraseHistorica.objects.filter(texto__icontains=termo)
+    paginacao = Paginator(frases, 10)
+
+    try:
+        pagina = paginacao.page(numero_pagina)
+    except EmptyPage:
+        frases = []
+    else:
+        frases = pagina.object_list
+
+    dados = [model_to_dict(frase) for frase in frases]
+    return JsonResponse({'frases': dados})
